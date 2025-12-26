bb07f6002a4ce7524e438340c28c9110efe70e85
Muhammad Taha Nasim <i240635@isb.nu.edu.pk>
2025-12-26 20:05:01 +0500

    Fix TypeScript error: Remove invalid 'smoothTouch' property from Lenis options

--- components/smooth-scroll/SmoothScroll.tsx
+++ components/smooth-scroll/SmoothScroll.tsx
@@ -17,7 +17,6 @@ export default function SmoothScroll({ children }: { children: React.ReactNode }
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      smoothTouch: false,
       touchMultiplier: 2,
       wheelMultiplier: 1,
     })
